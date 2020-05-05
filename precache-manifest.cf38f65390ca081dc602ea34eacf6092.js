0d6c015960f05a01e85cda818365f6d2a16904a7
amit <am.mail@gmail.com>
2020-05-05 22:30:57 +0530

    Updates

---
+++ precache-manifest.cf38f65390ca081dc602ea34eacf6092.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0e8b0b6837c5b585ace06812ccfc9e4",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "09b117262116b4da6dea",
+    "url": "/monsters-rolodex/static/css/main.e3eb0a80.chunk.css"
+  },
+  {
+    "revision": "a118fcff70fde5bdac91",
+    "url": "/monsters-rolodex/static/js/2.7bede9e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7bede9e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09b117262116b4da6dea",
+    "url": "/monsters-rolodex/static/js/main.e908bee2.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
